b82fe70ec131dca21106dd31b2d1c6d4e61a1210
hasansohailnaqi <hasansohail793@gmail.com>
2023-09-16 04:53:28 +0500

    processes

--- cc.ts
+++ cc.ts
@@ -2,8 +2,11 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 
+//currency converter api link
 
 let apilink = "https://v6.exchangerate-api.com/v6/9f6b43a8b33664d9be7d9425/latest/PKR";
+
+//fetching data
 
 let fetchData = async (data:any) => {
 let fetchData = await fetch(data)
@@ -13,15 +16,50 @@ return res.conversion_rates;
 
 let data = await fetchData(apilink);
 
+//object to array 
+
+ 
 let cuntries = Object.keys(data);
+
 
 let firstCuntry = await inquirer.prompt({
     type: "list",
     name: "name",
-    message: " please select first country..",
+    message: " converting from..",
+    choices: cuntries,
+});
+
+//user input first country
+
+let userMoney = await inquirer.prompt({
+    type: "number",
+    name: "money",
+    message: `please enter the amount in ${chalk.greenBright.bold(firstCuntry.name)}:`,
+});
+
+
+//convert country
+
+let secondCuntry = await inquirer.prompt({
+    type: "list",
+    name: "name",
+    message: " converting to..",
     choices: cuntries,
 });
 
 
+//conversion rate
 
+let cnv = `https://v6.exchangerate-api.com/v6/9f6b43a8b33664d9be7d9425/pair/${firstCuntry.name}/${secondCuntry.name}`;
 
+//fetching data for conversion rate
+
+let cnvData = async (data:any) => {
+let cnvData = await fetch(data)
+let res = await cnvData.json()
+return res.conversion_rate;
+};
+
+let a = await cnvData(cnv);
+
+console.log(a);
